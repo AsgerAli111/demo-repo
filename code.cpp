647b1d4ea6d0838e840f2140fdea4a340395c64b
AsgerAli <asger1312@gmail.com>
2024-02-28 10:03:17 +0530

    done all

--- code.cpp
+++ code.cpp
@@ -6,5 +6,7 @@ int main(){
   std::string name;
   std::cout<<" hello world";
   std::cin>>name;
+  std::cout<<" we have added feature successfully";
+  //testing the git commands here
   return 0;
 }
